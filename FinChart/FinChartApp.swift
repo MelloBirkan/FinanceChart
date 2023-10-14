cd2d76050e4b7048a05db56e5fcb01a42913601c
Mello Birkan <mgbirkan@icloud.com>
2023-10-13 23:53:20 -0300

    Initial Commit

---
+++ FinChart/FinChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinChartApp.swift
+//  FinChart
+//
+//  Created by Marcello Gonzatto Birkan on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FinChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
