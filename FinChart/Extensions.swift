7200700b32efde69636defe947f0c6a6264e8317
Mello Birkan <mgbirkan@icloud.com>
2024-02-05 03:45:55 -0300

    Cores adicionadas nos assets, e inicio da construção da tela inicial.

---
+++ FinChart/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  FinChart
+//
+//  Created by Marcello Gonzatto Birkan on 14/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  static let backgroundColor = Color(.background)
+  static let iconColor = Color(.icon)
+  static let textColor = Color(.text)
+  static let systemBackground = Color(uiColor: .systemBackground)
+}
